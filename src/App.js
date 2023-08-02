90db50c3c74a3a83d9285144b4196e3ed39c6f9a
Brevin Abraham <brevinabraham98@live.com>
2023-08-02 21:27:33 +0100

    Cleaned up backend data and switched to sending encrypted data instead of the entire chart. Utilized react-chartjs-2 to dynamically create a pie chart on the frontend using the received data. 📈 #DataEncryption #ReactChartJS

--- src/App.js
+++ src/App.js
@@ -4,17 +4,36 @@ import IconLinkedin from './Icon/IconLinkedIn.png'
 import IconBatman from './Icon/IconBatman.png'
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import { useState } from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 function App1() {
+  const [investmentData, setInvestmentData] = useState('');
   const [showChart, setShowChart] = useState(false);
-  const [chartImage, setChartImage] = useState('');
 
   const handleInvestmentsClick = () => {
     fetch('http://127.0.0.1:5000/return_investment_pie')
       .then((response) => response.json())
       .then((data) => {
-        setChartImage(data.chart);
-        setShowChart(true);
+        const decodedDataTickers = JSON.parse(JSON.parse(atob(data.tickerList)));
+        const decodedTotRtn = JSON.parse(JSON.parse(atob(data.totalRtn)));
+        const decodedtotalRtnPercentage = JSON.parse(JSON.parse(atob(data.totalRtnPercentage)));
+        const investmentDataExtracted = {'tickerList':{'ticker': [],'trueInitValue': [],'totRtn': [],'trueCurrValue': [],'perPie': [],'perTotRtn': [],'colour':[]},'totalRtn':decodedTotRtn,'totalRtnPercentage':decodedtotalRtnPercentage}
+        for (let i = 0; i < Object.keys(decodedDataTickers.ticker).length; i++) {
+          investmentDataExtracted.tickerList.ticker.push(decodedDataTickers.ticker[i])
+          investmentDataExtracted.tickerList.trueInitValue.push(decodedDataTickers.trueInitValue[i])
+          investmentDataExtracted.tickerList.totRtn.push(decodedDataTickers.totRtn[i])
+          investmentDataExtracted.tickerList.trueCurrValue.push(decodedDataTickers.trueCurrValue[i])
+          investmentDataExtracted.tickerList.perPie.push(decodedDataTickers.perPie[i])
+          investmentDataExtracted.tickerList.perTotRtn.push(decodedDataTickers.perTotRtn[i])
+          const red = Math.random()*255
+          const green = Math.random()*255
+          const blue = Math.random()*255
+          investmentDataExtracted.tickerList.colour.push(`rgb(${red},${green},${blue})`)
+        }
+        setShowChart(true)
+        setInvestmentData(investmentDataExtracted)
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
@@ -47,7 +66,17 @@ function App1() {
           </div>
           <div className='App-sub-main-body12'>
             {
-              showChart ? (<img src={`data:image/png;base64,${chartImage}`} alt='Pie Chart' style={{ width: '30vw', height: 'auto' }}/>):(<img src = {IconBatman} alt = 'WHERE IS SHE!!'style={{ width: '15vw', height: 'auto' }}/>)
+              showChart ? 
+              (<div className='App-sub-main-body12-pie-chart'>
+                <h2>Total Return: £{Number(investmentData.totalRtn).toFixed(2)} (+{Number(investmentData.totalRtnPercentage).toFixed(2)}%)</h2>
+                <Pie data = {{
+                  labels: investmentData.tickerList.ticker, datasets:[{
+                    data:investmentData.tickerList.trueCurrValue,backgroundColor:investmentData.tickerList.colour,borderColor: investmentData.tickerList.colour, borderWidth:0
+                    }]
+                  }}
+                />
+              </div>):
+              (<img src = {IconBatman} alt = 'WHERE IS SHE!!'style={{ width: '15vw', height: 'auto' }}/>)
             }
           </div>
         </div>
--- src/Investments/Data.py
+++ src/Investments/Data.py
@@ -1,10 +1,8 @@
 import requests
 import pandas as pd
 from flask import Flask, jsonify
-import matplotlib.pyplot as plt
-import io
-import base64
 import os
+import base64
 from dotenv import load_dotenv
 from flask_cors import CORS
 
@@ -12,9 +10,8 @@ load_dotenv()
 
 app = Flask(__name__)
 cors = CORS(app)
+
 @app.route('/return_investment_pie')
-
-
 def return_investment_pie():
     id = os.getenv("API_KEY")
     url = "https://live.trading212.com/api/v0/equity/portfolio"
@@ -27,7 +24,6 @@ def return_investment_pie():
     df_data = pd.DataFrame(data)
     pd.set_option('display.max_columns',None)
 
-
     df_data['initValue'] = df_data['quantity']*df_data['averagePrice']
     df_data['totRtn'] = df_data['ppl'] - df_data['fxPpl'].fillna(0)
     df_data['currValue'] = df_data['quantity']*df_data['currentPrice']
@@ -38,17 +34,10 @@ def return_investment_pie():
     # print(df_data[['ticker','trueInitValue','totRtn','trueCurrValue','perPie','perTotRtn']].sort_values(['totRtn'],ascending=False).reset_index(drop=True))
     # print(df_data['totRtn'].sum())
     # print("%.2f"%round(((df_data['currValue'].sum()/df_data['initValue'].sum())-1)*100,2))
-    plt.ioff()
-    plt.bar(df_data['ticker'], df_data['perTotRtn'])#.apply(lambda x: round(x,2)))
-    plt.xticks(rotation = 270)
-    buffer = io.BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-    chart_base64 = base64.b64encode(buffer.read()).decode('utf-8')
-    data = df_data['perPie']
-    labels = df_data['ticker']
-    plt.close()
-    return jsonify({"chart": chart_base64})
+    df_data_base64 = base64.b64encode(jsonify(df_data[['ticker','trueInitValue','totRtn','trueCurrValue','perPie','perTotRtn']].sort_values(['totRtn'],ascending=False).reset_index(drop=True).to_json()).data).decode('utf-8')
+    totalRtn_base64 = base64.b64encode(jsonify(df_data['totRtn'].sum()).data).decode('utf-8')
+    totalRtnPercentage_base64 = base64.b64encode(jsonify(round(((df_data['currValue'].sum()/df_data['initValue'].sum())-1)*100,2)).data).decode('utf-8')
+    return jsonify({"tickerList" : df_data_base64,"totalRtn":totalRtn_base64,"totalRtnPercentage":totalRtnPercentage_base64})
 
 if __name__ == '__main__':
     app.run(debug = True)
